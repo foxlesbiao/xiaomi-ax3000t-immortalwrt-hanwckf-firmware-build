6aecb1b04ebd5f3f2786a511c409a0421274a4f0
lesfox <addresseui@gmail.com>
2024-12-03 18:47:54 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -10,3 +10,4 @@
 
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git neko https://github.com/nosignals/openwrt-neko.git;main" >> "feeds.conf.default"
